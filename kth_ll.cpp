1badb39ca49d3fcb1969cade6256028d59e4fe56
Nitin Kumar Yadav <im_nky@yahoo.com>
2020-10-17 20:00:06 +0530

    Kth Element from back in linked list

---
+++ kth_ll.cpp
@@ -0,0 +1,20 @@
+/*Kth Elemenet from Last in Linkedlist*/
+
+// Iterative approach
+
+int getNode(SinglyLinkedListNode* head, int positionFromTail) {
+    int index = 0;
+    SinglyLinkedListNode * current = head;
+    SinglyLinkedListNode * result = head;
+    while(current!=NULL)
+    {
+        current=current->next;
+        if (index++>positionFromTail)
+        {
+            result=result->next;
+        }
+    }
+    return result->data;
+    
+    
+    
