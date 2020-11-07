a746419aa7956fec93d4386011665ba5a6653e6c
Nitin Kumar Yadav <im_nky@yahoo.com>
2020-11-07 19:05:31 +0530

    kth_node.cpp
    Kth node from back in Linked list

--- kth_ll.cpp
+++ kth_ll.cpp
@@ -20,7 +20,7 @@ int getNode(SinglyLinkedListNode* head, int positionFromTail) {
 // Recursive Approach
 int counter=0;
 int getNode(SinglyLinkedListNode* head, int positionFromTail) {
-    static int val=0;
+    //static int counter = 0;  /*When global variable is not allowed use local static */
     if(head!=NULL) {
         getNode(head->next,positionFromTail);
         counter = counter+1;
